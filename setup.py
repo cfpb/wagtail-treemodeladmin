bf58e87b82012a02b5834df670b25d22a0f5d709
Craig Davies <craig.davies@cfpb.gov>
2020-01-31 13:35:45 -0500

    Remove Django 2.1

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Wagtail',
         'Framework :: Wagtail :: 1',
