800b1e7eb13696953a2fa6f76f2968f2a346ae3f
Justin Slay <justin.slay@excella.com>
2022-04-13 13:40:26 -0600

    Added editorconfig, black, flake8, isort, pre-commit

--- treemodeladmin/tests/urls.py
+++ treemodeladmin/tests/urls.py
@@ -4,7 +4,8 @@ from wagtail.admin import urls as wagtailadmin_urls
 try:
     from django.urls import include, re_path
 except ImportError:
-    from django.conf.urls import include, url as re_path
+    from django.conf.urls import include
+    from django.conf.urls import url as re_path
 
 
 urlpatterns = [
